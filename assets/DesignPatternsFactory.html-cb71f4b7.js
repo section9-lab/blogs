4d8709c8651afce001e240fb59b6d5bfc9b0162a
section9-lab <section9lab@gmail.com>
2023-11-22 06:31:35 +0000

    Deploying to gh-pages from @ section9-lab/blogs@9c3a2d40514dd338daaa9ebb4e6072933a7d2b50 🚀

---
+++ assets/DesignPatternsFactory.html-cb71f4b7.js
@@ -0,0 +1,82 @@
+import{_ as e}from"./plugin-vue_export-helper-c27b6911.js";import{o as i,c as n,f as a}from"./app-dd70a414.js";const s={},d=a(`<h1 id="design-patterns-overview" tabindex="-1"><a class="header-anchor" href="#design-patterns-overview" aria-hidden="true">#</a> Design Patterns Overview</h1><h1 id="一、创建型模式" tabindex="-1"><a class="header-anchor" href="#一、创建型模式" aria-hidden="true">#</a> 一、创建型模式：</h1><h2 id="_2、工厂模式" tabindex="-1"><a class="header-anchor" href="#_2、工厂模式" aria-hidden="true">#</a> 2、工厂模式</h2><h3 id="简单工厂" tabindex="-1"><a class="header-anchor" href="#简单工厂" aria-hidden="true">#</a> 简单工厂</h3><figure><img src="https://kroki.io/plantuml/svg/eNqlU81KAzEQvucp5liRBi966qGi9AFsb-JhSKZx2PyZTBVp67O73S3SQ-mu9BLCwPc7ybwKFtkEr2rDMWPBAAVjUynD_d3J0GaGh9NBRtOgoxWLp0fPLgaKAp7WopTxWCuYFLRQFW2wUtUhWfJ61Q6eKSTYqlvYturCZg-fiS0E5DhZSuHoXt9u1F5xFCprNHSeqr5jJr08nC3bT09iC35NDuALHnrgExfjqfMxGtmJLdBIKt8dsld3JN3l6H6U_AsZwej-6-AY-mOD5Ryy7X6oqtluOh3u5Xqev4DXU_Vx1eUHNdtpPby1MWbG8ICaU7Ttx_kFgWYoOQ==" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>─model
+    │  ShapeFactory.java
+    │  TestDemo.java
+    │
+    └─shape
+            Circle.java
+            Rectangle.java
+            Shape.java
+            Square.java
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>public interface Shape</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>package com.test.cases.model.shape;
+
+public interface Shape {
+    void draw();
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>public class Square implements Shape</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>package com.test.cases.model.shape;
+
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println(&quot;Shape Square draw&quot;);
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>public class Rectangle implements Shape</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>package com.test.cases.model.shape;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println(&quot;Shape Rectangle draw&quot;);
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>public class Circle implements Shape</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>package com.test.cases.model.shape;
+
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println(&quot;Shape Circle draw&quot;);
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>public class ShapeFactory</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>package com.test.cases.model;
+
+import com.test.cases.model.shape.Circle;
+import com.test.cases.model.shape.Rectangle;
+import com.test.cases.model.shape.Shape;
+import com.test.cases.model.shape.Square;
+
+public class ShapeFactory {
+    public Shape getShape(String shapeType){
+        if(shapeType == null){
+            return null;
+        }
+        if(shapeType.equalsIgnoreCase(&quot;CIRCLE&quot;)){
+            return new Circle();
+        } else if(shapeType.equalsIgnoreCase(&quot;RECTANGLE&quot;)){
+            return new Rectangle();
+        } else if(shapeType.equalsIgnoreCase(&quot;SQUARE&quot;)){
+            return new Square();
+        }
+        return null;
+    }
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><p>public class TestDemo</p><div class="language-markdown line-numbers-mode" data-ext="md"><pre class="language-markdown"><code>package com.test.cases.model;
+
+import com.test.cases.model.shape.Shape;
+
+public class TestDemo {
+    public static void main(String[] args) {
+        ShapeFactory shapeFactory = new ShapeFactory();
+
+<span class="token code keyword">        //获取 Circle 的对象，并调用它的 draw 方法
+        Shape circle = shapeFactory.getShape(&quot;CIRCLE&quot;);
+        circle.draw();</span>
+
+<span class="token code keyword">        //获取 Rectangle 的对象，并调用它的 draw 方法
+        Shape rectangle = shapeFactory.getShape(&quot;RECTANGLE&quot;);
+        rectangle.draw();</span>
+
+<span class="token code keyword">        //获取 Square 的对象，并调用它的 draw 方法
+        Shape square = shapeFactory.getShape(&quot;SQUARE&quot;);
+        square.draw();
+    }</span>
+}
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="抽象工厂" tabindex="-1"><a class="header-anchor" href="#抽象工厂" aria-hidden="true">#</a> 抽象工厂</h3><figure><img src="https://kroki.io/plantuml/svg/eNq1VkFOwzAQvPsVPoKq-AUcCkVwRS03hJBxtsGSa5e1A0JteTuOnVRpRaXYUS6JlWxmZse768yt4-jqjSJSO8A1F0ANVqwEKyv9tuXOP9WW8XfrkAvnA5zBHyaMMsgWzZXuyC_9MrKka6nU1TU5EKG4tQk4d6oGDzPLhnkG6-5hYwLIzufkpDhEtA2X-mrlUOrq5TUN9SHen9CUtQA8Bb9tg9sgWkG3bNlyjFhCOcqH1QffQqeoAQoPGm1hcVQ2o3Hr_Iuw6EnuIOlw1nMrgk9d0OECUz_kgsqUzG3zJVtIFKpXSiXy70QLg8pxFqa1UlQeWY6tlCE8VtAjAuhRNRT1LEE4rquRZra5fdYc_wPKKa6bfVEk98K0TP2SyWMq6m2RM5JIzhgbznYcq4SkHgrDSZLdO6HZM5Z40EzN4Wf41BShyYftSX-6DN-T5N45oRmazNmkmZoojqGpWeIRRMgcdOn_q_4AyO2Gqg==" alt="" tabindex="0" loading="lazy"><figcaption></figcaption></figure>`,20),l=[d];function r(c,v){return i(),n("div",null,l)}const m=e(s,[["render",r],["__file","DesignPatternsFactory.html.vue"]]);export{m as default};
